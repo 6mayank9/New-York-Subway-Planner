b0cafc0bdee81bb14e94312946f1ade00a91f6dc
6mayank9 <mayanksingh2007@gmail.com>
2017-03-12 15:37:45 -0400

    Added Subway Lines A-C

--- SubwayGraph/Dummy/SubwayLinesCsvGenerator.py
+++ SubwayGraph/Dummy/SubwayLinesCsvGenerator.py
@@ -1,13 +1,13 @@
 import csv
 
 
-list = ["A","B","C","D","E","F"]
+list = ["D"]
 with open('SubwayStops.csv') as csvfile:
     readCsv = csv.reader(csvfile, delimiter=',')
     for row in readCsv:
         for k in list:
             if( str(k) in row[5].split(" ")):
-                with open('LineA-F.csv','a') as writefile:
+                with open('LineD.csv','a') as writefile:
                     for l in row:
                         writefile.write(l+",")
                     writefile.write("\n")
