b65ce77217140a9859d55124459d824233edd097
6mayank9 <mayanksingh2007@gmail.com>
2017-03-22 22:40:00 -0400

    Started working on weighted edges

--- loadData.py
+++ loadData.py
@@ -1,6 +1,9 @@
 '''Example of how to load the graph and dictionary
 '''
 import pickle
+import realtime_data as rd
+import time
+rd.loopdwlmta(1)
 class station:                                # station class to store attributes of each subway station
     def __init__(self, stop_id, stop_name, trains):
         self.id = stop_id                     # Unique Station ID
@@ -8,8 +11,15 @@ class station:                                # station class to store attribute
         self.trains = trains                  # List of trains at this staion
 filehandler = open("alldumps.pkl","rb")
 subwayDictionary,G = pickle.load(filehandler)
-
 k = G.neighbors(subwayDictionary["104"])
-print subwayDictionary["104"].name+" number of neighbors are ",len(k)
-for i in range(len(k)):
-    print k[i].name
+#print subwayDictionary["104"].name+" number of neighbors are ",len(k)
+#for i in range(len(k)):
+#    print k[i].name
+
+#time.sleep(5)
+with open("realtimedata.txt",mode='r') as file:
+    for line in file:
+        content = line.split(" ")
+        print line
+
+
--- realtime_data.py
+++ realtime_data.py
@@ -3,17 +3,22 @@ from google.transit import gtfs_realtime_pb2
 import urllib
 import datetime
 import time
-
-def savefile(listitems):
-    with open('mta.txt' ,'a') as f:
-        f.write(listitems + '\n')
+import pickle
+class station:                                # station class to store attributes of each subway station
+    def __init__(self, stop_id, stop_name, trains):
+        self.id = stop_id                     # Unique Station ID
+        self.name = stop_name                 # Station name
+        self.trains = trains                  # List of trains at this staion
+filehandler = open("alldumps.pkl","rb")
+subwayDictionary,G = pickle.load(filehandler)
+newfile = open('realtimedata.txt' ,'w')
 
 def timediff(s1,s2):
     from datetime import datetime
     FMT = '%H:%M:%S'
     tdelta = datetime.strptime(s2, FMT) - datetime.strptime(s1, FMT)
     return tdelta
-
+realtimedata = []
 def loopdwlmta(x):	## send request to api mta.info using my own key
     feed = gtfs_realtime_pb2.FeedMessage()
     response = urllib.urlopen('http://datamine.mta.info/mta_esi.php?key=44fe903c249d311125b5bc56d79ab7ac')
@@ -41,27 +46,34 @@ def loopdwlmta(x):	## send request to api mta.info using my own key
                 stop_id = entity.trip_update.stop_time_update[j].stop_id
                 station_nth = j
                 tripcomplete.extend([tmpSys,TripIdi, Routei, station_nth, stop_id, ArrivalTime] )
-                print tmpSys[11:19],"   ",TripIdi,"   ",stop_id,"   ",time.ctime(ArrivalTime)[11:]
+                #print tmpSys[11:19],"   ",TripIdi,"   ",stop_id,"   ",time.ctime(ArrivalTime)[11:]
+                if(time.ctime(ArrivalTime)[20:]!="1969"):
+                    tripstr = [TripIdi,stop_id,time.ctime(ArrivalTime)[11:19]]
+                    newfile.write(tripstr[0]+" "+tripstr[1]+" "+tripstr[2])
+                    newfile.write("\n")
+                realtimedata.append([tripstr[0],tripstr[1],tripstr[2]])
                 s1 = tmpSys[11:19]
                 s2 = time.ctime(ArrivalTime)[11:19]
-                print timediff(s1,s2)
-                #print tripcomplete
-                #print tripcomplete
-                tripcomplstr = ','.join([str(i) for i in tripcomplete])
+                #print timediff(s1,s2)
 
-                savefile(tripcomplstr)
+
                 j+=1
 
 
 
-
-while True:
+"""while True:
     try:
         loopdwlmta(1)
     except:
         continue
     time.sleep(30)
-
+"""
+loopdwlmta(1)
+file = open("stopTimesData.pkl","rb")
+stopTimesData = pickle.load(file)
+for i in realtimedata:
+    if(i[0] in stopTimesData):
+        print "TripId: "+i[0]+" Station ID: "+i[1]+" Original Time: "+stopTimesData[i[0]][i[1]]+" Current Arrival Time: "+i[2]
 
 
 
