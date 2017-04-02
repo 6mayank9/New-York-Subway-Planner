04b3bcfa44a0f6c73f21f8e3a2b1089a722579d9
6mayank9 <mayanksingh2007@gmail.com>
2017-04-02 19:57:59 -0400

    Optimzed realtimeData

--- realtime_data.py
+++ realtime_data.py
@@ -22,36 +22,37 @@ def timediff(s1,s2):
     return tdelta.total_seconds()
 realtimedata = []
 def loopdwlmta(x):	## send request to api mta.info using my own key
-    '''Lines 1-6,S'''
+    '''All Lines'''
     feed = gtfs_realtime_pb2.FeedMessage()
-    response = urllib.urlopen('http://datamine.mta.info/mta_esi.php?key=44fe903c249d311125b5bc56d79ab7ac&feed_id=1')
-    feed.ParseFromString(response.read())
-    ## looping the content downloaded
-    tmpSys = datetime.datetime.strftime(datetime.datetime.now() ,'%Y-%m-%d %H:%M:%S')
-    i = 1
+    response = [urllib.urlopen('http://datamine.mta.info/mta_esi.php?key=44fe903c249d311125b5bc56d79ab7ac&feed_id=1'),urllib.urlopen('http://datamine.mta.info/mta_esi.php?key=44fe903c249d311125b5bc56d79ab7ac&feed_id=16'),urllib.urlopen('http://datamine.mta.info/mta_esi.php?key=44fe903c249d311125b5bc56d79ab7ac&feed_id=21')]
+    for t in range(len(response)):
 
-    for entity in feed.entity:
-        if entity.HasField('trip_update'):
-            TripIdi = entity.trip_update.trip.trip_id
-            Routei = entity.trip_update.trip.route_id
-            p3trip = min(len(entity.trip_update.stop_time_update),3)
+        feed.ParseFromString(response[t].read())
+        ## looping the content downloaded
+        tmpSys = datetime.datetime.strftime(datetime.datetime.now() ,'%Y-%m-%d %H:%M:%S')
 
-            j=0
-            while j < p3trip:
-                tripcomplete = []
-                ArrivalTime = entity.trip_update.stop_time_update[j].arrival.time
-                stop_id = entity.trip_update.stop_time_update[j].stop_id
-                station_nth = j
-                tripcomplete.extend([tmpSys,TripIdi, Routei, station_nth, stop_id, ArrivalTime] )
-                #print tmpSys[11:19],"   ",TripIdi,"   ",stop_id,"   ",time.ctime(ArrivalTime)[11:]
-                if(time.ctime(ArrivalTime)[20:]!="1969"):
-                    tripstr = [TripIdi,stop_id,time.ctime(ArrivalTime)[11:19]]
-                    newfile.write(tripstr[0]+" "+tripstr[1]+" "+tripstr[2])
-                    newfile.write("\n")
-                realtimedata.append([tripstr[0],tripstr[1],tripstr[2]])
-                #print TripIdi+" "+stop_id+" "+tripstr[2]
-                j+=1
-    '''Lines N,Q,R,W'''
+
+        for entity in feed.entity:
+            if entity.HasField('trip_update'):
+                TripIdi = entity.trip_update.trip.trip_id
+                Routei = entity.trip_update.trip.route_id
+                p3trip = min(len(entity.trip_update.stop_time_update),3)
+
+                j=0
+                while j < p3trip:
+                    tripComplete = []
+                    ArrivalTime = entity.trip_update.stop_time_update[j].arrival.time
+                    stop_id = entity.trip_update.stop_time_update[j].stop_id
+                    station_nth = j
+                    tripComplete.extend([tmpSys,TripIdi, Routei, station_nth, stop_id, ArrivalTime] )
+                    if time.ctime(ArrivalTime)[20:]!="1969":
+                        tripstr = [TripIdi,stop_id,time.ctime(ArrivalTime)[11:19]]
+                        newfile.write(tripstr[0]+" "+tripstr[1]+" "+tripstr[2])
+                        newfile.write("\n")
+                    realtimedata.append([tripstr[0],tripstr[1],tripstr[2]])
+                    print t
+                    j += 1
+    '''
     response = urllib.urlopen('http://datamine.mta.info/mta_esi.php?key=44fe903c249d311125b5bc56d79ab7ac&feed_id=16')
     feed.ParseFromString(response.read())
     tmpSys = datetime.datetime.strftime(datetime.datetime.now(), '%Y-%m-%d %H:%M:%S')
@@ -78,7 +79,7 @@ def loopdwlmta(x):	## send request to api mta.info using my own key
                 realtimedata.append([tripstr[0], tripstr[1], tripstr[2]])
                 #print TripIdi+" "+stop_id+" "+tripstr[2]
                 j += 1
-    '''Lines B,D'''
+    
     response = urllib.urlopen('http://datamine.mta.info/mta_esi.php?key=44fe903c249d311125b5bc56d79ab7ac&feed_id=21')
     feed.ParseFromString(response.read())
     tmpSys = datetime.datetime.strftime(datetime.datetime.now(), '%Y-%m-%d %H:%M:%S')
@@ -105,7 +106,7 @@ def loopdwlmta(x):	## send request to api mta.info using my own key
                 realtimedata.append([tripstr[0], tripstr[1], tripstr[2]])
                 print TripIdi + " " + stop_id + " " + tripstr[2]
                 j += 1
-
+'''
 """while True:
     try:
         loopdwlmta(1)
