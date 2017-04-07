1943f9eaad55c94cc1f7e536fd0ab6ca3a8c0e99
6mayank9 <mayanksingh2007@gmail.com>
2017-04-07 13:40:00 -0400

    Updated graph

--- SubwayGraph/SubwayGraph.py
+++ SubwayGraph/SubwayGraph.py
@@ -16,33 +16,48 @@ def adj_list_to_file(B,file_name):
             f.write(neighbor + ' ')
         f.write('\n')
 class station:                                # station class to store attributes of each subway station
-    def __init__(self, stop_id, stop_name, trains):
+    def __init__(self, stop_id, stop_name, trains, latitude, longitude):
         self.id = stop_id                     # Unique Station ID
         self.name = stop_name                 # Station name
         self.trains = trains                  # List of trains at this staion
+        self.longitude = longitude
+        self.latitude = latitude
 check = 201
 G = nx.Graph()                                # Undirected graph
 stations = []
 subwayDictionary = {}                         # Dictionary to get station object from station id
-b = station("a", "dummyStation", "-A")
+line = str(1)
+check = ["201","301","401","501","601","901",""]
+p=0
+b = station("a", "dummyStation", "-A", 0, 0)
 '''Lines 1-6 & S Graph '''
 with open('Lines1-6.csv') as csvfile:
     readCsv = csv.reader(csvfile, delimiter=',')
     readCsv.next()
     for row in readCsv:
-        if(str(row[0]) in subwayDictionary):
-            a = subwayDictionary[str(row[0])]
-        else:
-            a = station(row[0],row[1],row[5])
-            subwayDictionary[str(row[0])] = a
-            stations.append(a)
-        if(a.id!=str(check)):
+        row[0] = str(str(row[0])+line)
+        #print row[0]
+        a = station(row[0],row[1],row[5], row[2], row[3])
+        subwayDictionary[str(row[0])] = a
+        stations.append(a)
+        for h in ["1","2","3","4","5","6","S"]:
+            if (str(str(row[0][:3])+h) in subwayDictionary) and (str(str(row[0][:3])+h) != str(row[0])):
+                connect = subwayDictionary[str(str(row[0][:3])+h)]
+                G.add_edge(a.id,connect.id, weight = 0)
+        if(a.id[:3]!=str(check[p])):
             G.add_edge(a.id, b.id, weight=1)                  # Add weight of edges here if required or later when reading gpickle file
         else:
-            if check == 601:
-                check = 901
-            else:
-                check += 100
+            p += 1
+
+            subwayDictionary.pop(str(row[0]))
+
+            line = str(int(line)+1)
+            if line == "7":
+               line = "S"
+            a.id = a.id[:3]+line
+            subwayDictionary[a.id] = a
+        #print a.id+" "+subwayDictionary[a.id].name
+
 
         b = a
 G.remove_node("a")
@@ -51,19 +66,22 @@ for i in ["B","D","N","Q","R","W"]:
     with open('Line'+i+'.csv') as file:
         readCsv = csv.reader(file, delimiter=',')
         temp = readCsv.next()
-        c = station(temp[0],temp[1],temp[5])
-        subwayDictionary[str(temp[0])] = c
+        c = station(str(temp[0]+i),temp[1],temp[5], row[2], row[3])
+        subwayDictionary[str(temp[0]+i)] = c
         for row in readCsv:
-            if(str(row[0]) in subwayDictionary):
-                d = subwayDictionary[str(row[0])]
-            else:
-                d = station(str(row[0]),row[1],row[5])
+                row[0] = row[0]+i
+
+                d = station(str(row[0]),row[1],row[5], row[2], row[3])
+                for h in ["1", "2", "3", "4", "5", "6", "S","B","D","N","Q","R","W"]:
+                    if (str(str(row[0][:3]) + h) in subwayDictionary) and (str(str(row[0][:3]) + h) != str(row[0])):
+                        connect = subwayDictionary[str(str(row[0][:3]) + h)]
+                        G.add_edge(a.id, connect.id, weight=0)
 
                 subwayDictionary[str(row[0])] = d
                 stations.append(a)
-            G.add_edge(d.id,c.id, weight =1)
-            c=d
-'''Connecting Same Station with multiple ids'''
+                G.add_edge(d.id,c.id, weight =1)
+                c=d
+'''Connecting Same Station with multiple ids
 G.add_edge("901","631",weight = 0)  #Grand Central
 G.add_edge("127","902",weight = 0)  #Times Sq
 G.add_edge("R16","901",weight = 0)  #Times Sq
@@ -73,8 +91,30 @@ G.add_edge("R23","Q01",weight = 0)  #Canal St
 G.add_edge("639","R01",weight = 0)  #Canal St
 G.add_edge("235","D24",weight = 0)  #Atlantic Av-Barclays
 G.add_edge("R31","235",weight = 0)  #Atlantic Av-Barclays
-G.add_edge("A24","125",weight = 0)  #58 St - Columbus Circle
-
+G.add_edge("A24","125",weight = 0)  #58 St - Columbus Circle  
+'''
+for i in ["1", "2", "3", "4", "5", "6", "S","B","D","N","Q","R","W"]:
+    for j in ["1", "2", "3", "4", "5", "6", "S","B","D","N","Q","R","W"]:
+        if (str("901"+i) in subwayDictionary) and (str("631"+j) in subwayDictionary):
+            G.add_edge(str("901"+i),str("631"+j), weight = 0)
+        if (str("127" + i) in subwayDictionary) and (str("902" + j) in subwayDictionary):
+            G.add_edge(str("127" + i), str("902" + j), weight=0)
+        if (str("R16" + i) in subwayDictionary) and (str("901" + j) in subwayDictionary):
+            G.add_edge(str("R16" + i), str("901" + j), weight=0)
+        if (str("R17" + i) in subwayDictionary) and (str("D17" + j) in subwayDictionary):
+            G.add_edge(str("R17" + i), str("D17" + j), weight=0)
+        if (str("R20" + i) in subwayDictionary) and (str("635" + j) in subwayDictionary):
+            G.add_edge(str("R20" + i), str("635" + j), weight=0)
+        if (str("R23" + i) in subwayDictionary) and (str("Q01" + j) in subwayDictionary):
+            G.add_edge(str("R23" + i), str("Q01" + j), weight=0)
+        if (str("639" + i) in subwayDictionary) and (str("R01" + j) in subwayDictionary):
+            G.add_edge(str("639" + i), str("R01" + j), weight=0)
+        if (str("235" + i) in subwayDictionary) and (str("D24" + j) in subwayDictionary):
+            G.add_edge(str("235" + i), str("D24" + j), weight=0)
+        if (str("R31" + i) in subwayDictionary) and (str("235" + j) in subwayDictionary):
+            G.add_edge(str("R31" + i), str("235" + j), weight=0)
+        if (str("A24" + i) in subwayDictionary) and (str("125" + j) in subwayDictionary):
+            G.add_edge(str("A24" + i), str("125" + j), weight=0)
 
 import pickle
 newfile = open("alldumps.pkl","wb")           # Dumps the graph and dictionary to a file
@@ -90,10 +130,9 @@ for i in range(len(k)):
 nx.draw(G)
 #print nx.is_connected(G)
 D = list(nx.connected_component_subgraphs(G)) # Get the list of connected components
-print "Total connected components are: ",len(D)
-print subwayDictionary["Q05"].name
+print subwayDictionary["6016"].name
 #for i in range(len(D[0])):
-#    print D[0].nodes()[i].id
+#    print subwayDictionary[D[0].nodes()[i]].name
 #adj_list_to_file(G,"tst.txt")
 
 jsondump = json_graph.node_link_data(G)
--- realtime_data.py
+++ realtime_data.py
@@ -24,7 +24,7 @@ realtimedata = []
 def loopdwlmta(x):	## send request to api mta.info using my own key
     '''All Lines'''
     feed = gtfs_realtime_pb2.FeedMessage()
-    response = [urllib.urlopen('http://datamine.mta.info/mta_esi.php?key=44fe903c249d311125b5bc56d79ab7ac&feed_id=1'),urllib.urlopen('http://datamine.mta.info/mta_esi.php?key=44fe903c249d311125b5bc56d79ab7ac&feed_id=16'),urllib.urlopen('http://datamine.mta.info/mta_esi.php?key=44fe903c249d311125b5bc56d79ab7ac&feed_id=21')]
+    response = [urllib.urlopen('http://datamine.mta.info/mta_esi.php?key=44fe903c249d311125b5bc56d79ab7ac&feed_id=1'),urllib.urlopen('http://datamine.mta.info/mta_esi.php?key=44fe903c249d311125b5bc56d79ab7ac&feed_id=21')]
     for t in range(len(response)):
 
         feed.ParseFromString(response[t].read())
@@ -50,7 +50,7 @@ def loopdwlmta(x):	## send request to api mta.info using my own key
                         newfile.write(tripstr[0]+" "+tripstr[1]+" "+tripstr[2])
                         newfile.write("\n")
                     realtimedata.append([tripstr[0],tripstr[1],tripstr[2]])
-                    print t
+
                     j += 1
 
 """while True:
@@ -68,18 +68,25 @@ for i in realtimedata:
         s1 = stopTimesData[i[0]][i[1]]
         s2 = i[2]
         delay = float(timediff(s1, s2))
-        #print "TripId: "+i[0]+" Station ID: "+i[1]+" Original Time: "+stopTimesData[i[0]][i[1]]+" Current Arrival Time: "+i[2]+" Delay: ", delay
+        print "TripId: "+i[0]+" Station ID: "+i[1]+" Original Time: "+stopTimesData[i[0]][i[1]]+" Current Arrival Time: "+i[2]+" Delay: ", delay
         if delay > 0 :
-            k = G.neighbors(i[1][0:3])
-            for j in range(len(k)):
-                if(G[i[1][0:3]][k[j]]['weight'] != 0):
-                    G[i[1][0:3]][k[j]]['weight'] += delay
+            if i[0][7] == "G":
+                i[0][7]="S"
+            print str(str(i[1][0:3])+i[0][7])
+            if(str(str(i[1][0:3])+i[0][7]) in subwayDictionary):
+                print "found"
+                k = G.neighbors(str(i[1][0:3])+i[0][7])
+                for j in range(len(k)):
+                    if(G[str(i[1][0:3])+i[0][7]][k[j]]['weight'] != 0):
+                        G[str(i[1][0:3])+i[0][7]][k[j]]['weight'] += delay
+
+
 print "Weights Updated"
 print "Calculating Route...."
-route =  nx.dijkstra_path(G,"D07","257",'weight')
+route =  nx.dijkstra_path(G,"1201","1202",'weight')
 for s in route:
     print subwayDictionary[s].name
-print nx.dijkstra_path_length(G,"D07","257",'weight')
+print nx.dijkstra_path_length(G,"1201","1202",'weight')
 
 
 
